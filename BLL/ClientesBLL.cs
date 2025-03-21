68e279bf3809219b45f79eb30c3a7f465272a7b8
Leticia <gracavitoriareisjunho.git@gmail.com>
2025-03-21 17:48:52 -0400

    Atualização de arquivos

---
+++ BLL/ClientesBLL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Modelos;
+using DAL;
+using Microsoft.Data.SqlClient;
+using Microsoft.Data;
+
+namespace BLL
+{
+    public class ClientesBLL
+    {
+
+    }
+}
