f877bf71e7b901689b5affa47fdc123783e243d0
Leticia <gracavitoriareisjunho.git@gmail.com>
2025-03-24 14:58:16 -0400

    arquivos

--- BLL/ClientesBLL.cs
+++ BLL/ClientesBLL.cs
@@ -12,6 +12,19 @@ namespace BLL
 {
     public class ClientesBLL
     {
+        //o nome do cliente é obrigatório
 
+        public void Incluir(ClienteInformation cliente)
+        {
+            if (cliente.Nome.Trim().Length == 0)
+            {
+                throw new Exception("O nome do cliente é obrigatório");
+            }
+            //E-mail é sempre com letras minúsculas
+            cliente.Email = cliente.Email.ToLower();
+            //se tudo está OK, chama a rotina para inserir
+            ClienteDal obj = new ClienteDal();
+            obj.Incluir(cliente);
+        }
     }
 }
